2b8cb8197a7579638462ad54e5089c66a6de6512
danielbukowski <daniel.bukowski01@gmail.com>
2023-03-07 20:01:29 +0100

    initial commit

---
+++ src/test/java/com/danielbukowski/photosharing/PhotoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielbukowski.photosharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
