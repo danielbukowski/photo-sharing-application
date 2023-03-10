371fca58deb468f0933322440fbc88ef06f971de
danielbukowski <82054911+danielbukowski@users.noreply.github.com>
2023-03-10 09:11:52 +0100

    Revert "Added get, create, delete methods for the accountController"

--- src/main/java/com/danielbukowski/photosharing/PhotoSharingApplication.java
+++ src/main/java/com/danielbukowski/photosharing/PhotoSharingApplication.java
@@ -2,9 +2,11 @@ package com.danielbukowski.photosharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
 		SecurityAutoConfiguration.class
 })
 public class PhotoSharingApplication {
