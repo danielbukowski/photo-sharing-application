e1cda34866917a6f2848363ce2ee380a21e6a91a
danielbukowski <daniel.bukowski01@gmail.com>
2023-03-12 09:24:55 +0100

    Add ResponseEntity and Validation features to AccountController

--- src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
+++ src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
@@ -1,12 +1,21 @@
 package com.danielbukowski.photosharing.Dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountRegisterRequest {
 
-
+    @NotEmpty(message = "Login should not be empty")
     private final String login;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$",
+            message = "Password must contain one digit from 1 to 9, " +
+                    "one lowercase letter, " +
+                    "one uppercase letter, " +
+                    "one special character, " +
+                    "no space, " +
+                    "and it must be 8-32 characters long.")
     private final String password;
 
 }
