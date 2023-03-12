f28b99093ce1296162f6880ba9ec558ee29417cb
danielbukowski <daniel.bukowski01@gmail.com>
2023-03-12 20:19:43 +0100

    Fix AccountRegisterRequest validation and change CreateAccount method's response

--- src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
+++ src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
@@ -9,6 +9,7 @@ public class AccountRegisterRequest {
 
     @NotEmpty(message = "Login should not be empty")
     private final String login;
+    @NotEmpty(message = "Password should not be empty")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$",
             message = "Password must contain one digit from 1 to 9, " +
                     "one lowercase letter, " +
