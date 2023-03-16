50b124577189e0f74b82540a8506d70aaf5bc373
danielbukowski <daniel.bukowski01@gmail.com>
2023-03-17 00:33:27 +0100

    Add authorization and authentication access denied handlings

---
+++ src/main/java/com/danielbukowski/photosharing/Handler/AuthorizationDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.danielbukowski.photosharing.Handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AuthorizationDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+            throws IOException {
+        response.sendError(HttpStatus.FORBIDDEN.value(), "You don't have access to this resource");
+    }
+}
