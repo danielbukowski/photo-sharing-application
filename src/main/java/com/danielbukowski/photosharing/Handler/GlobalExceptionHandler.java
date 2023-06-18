7428341a2f4daf5b6d9e0f949191fca89e29a1ae
danielbukowski <daniel.bukowski01@gmail.com>
2023-06-19 00:38:48 +0200

    Improve Exception Handler

--- src/main/java/com/danielbukowski/photosharing/Handler/GlobalExceptionHandler.java
+++ src/main/java/com/danielbukowski/photosharing/Handler/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package com.danielbukowski.photosharing.Handler;
 
 import com.danielbukowski.photosharing.Dto.ExceptionResponse;
 import com.danielbukowski.photosharing.Dto.ValidationExceptionResponse;
+import com.danielbukowski.photosharing.Exception.AccountAlreadyExistsException;
+import com.danielbukowski.photosharing.Exception.AccountNotFoundException;
+import com.danielbukowski.photosharing.Exception.ImageNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -19,11 +22,26 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleExceptions(RuntimeException ex) {
         var bodyResponse = ExceptionResponse.builder()
                 .timestamp(LocalDateTime.now())
-                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                .reason(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString())
+                .build();
+
+        return new ResponseEntity<>(
+                bodyResponse,
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(AccountAlreadyExistsException.class)
+    public ResponseEntity<?> handleBadRequestExceptions(RuntimeException ex) {
+        var bodyResponse = ExceptionResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.name())
                 .reason(ex.getMessage())
                 .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString())
                 .build();
@@ -31,6 +49,21 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(
                 bodyResponse,
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({AccountNotFoundException.class, ImageNotFoundException.class})
+    public ResponseEntity<?> handleNotFoundExceptions(RuntimeException ex) {
+        var bodyResponse = ExceptionResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .reason(ex.getMessage())
+                .path(ServletUriComponentsBuilder.fromCurrentRequest().toUriString())
+                .build();
+
+        return new ResponseEntity<>(
+                bodyResponse,
+                HttpStatus.NOT_FOUND
         );
     }
 
