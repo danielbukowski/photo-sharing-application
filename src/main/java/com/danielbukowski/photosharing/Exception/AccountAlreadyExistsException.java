dde1681d96fc4b5fe64520a747a6ea4aaa3cb143
danielbukowski <daniel.bukowski01@gmail.com>
2023-06-19 00:25:25 +0200

    Add the AccountAlreadyExistsException

---
+++ src/main/java/com/danielbukowski/photosharing/Exception/AccountAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.danielbukowski.photosharing.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AccountAlreadyExistsException extends RuntimeException {
+
+    public AccountAlreadyExistsException(String message) {
+        super(message);
+    }
+}
