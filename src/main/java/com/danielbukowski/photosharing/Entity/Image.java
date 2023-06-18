df9eebd8419764f3710b955bbe51a56850515a83
danielbukowski <daniel.bukowski01@gmail.com>
2023-06-19 00:29:23 +0200

    Rename images' field name and improve the equals and hashCode methods

--- src/main/java/com/danielbukowski/photosharing/Entity/Image.java
+++ src/main/java/com/danielbukowski/photosharing/Entity/Image.java
@@ -8,12 +8,12 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,18 +27,48 @@ public class Image {
     )
     private UUID id;
 
-    @Column(unique = true)
+    @Column(
+            unique = true
+    )
     private String path;
 
-    @Column(updatable = false, nullable = false)
+    @Column(
+            updatable = false,
+            nullable = false
+    )
     private String title;
 
-    @Column(updatable = false, nullable = false)
+    @Column(
+            updatable = false,
+            nullable = false
+    )
     @Enumerated(EnumType.STRING)
     private FileExtension extension;
 
     @CreatedDate
-    @Column(updatable = false, nullable = false)
+    @Column(
+            updatable = false,
+            nullable = false
+    )
     private LocalDateTime createdDate;
 
+    @ManyToOne(
+            fetch = FetchType.LAZY,
+            optional = false
+    )
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return Objects.equals(id, image.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 }
--- src/main/resources/db/migration/V2__create_table_images.sql
+++ src/main/resources/db/migration/V2__create_table_images.sql
@@ -3,8 +3,8 @@ CREATE TABLE images (
    account_id UUID,
    path VARCHAR(255) UNIQUE,
    title VARCHAR(255) NOT NULL ,
-   file_extension VARCHAR(4) NOT NULL,
-   created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+   extension VARCHAR(4) NOT NULL,
+   created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
    CONSTRAINT pk_images PRIMARY KEY (id),
    CONSTRAINT fk_images_on_account FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
