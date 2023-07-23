fafbd4c97994bd86aae7f149d78ada5fff2d1158
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:22:50 +0200

    Move AccountController to v2 and refactor

--- src/main/java/com/danielbukowski/photosharing/Controller/AccountController.java
+++ src/main/java/com/danielbukowski/photosharing/Controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.danielbukowski.photosharing.Controller;
 
-import com.danielbukowski.photosharing.Dto.AccountDto;
 import com.danielbukowski.photosharing.Dto.AccountRegisterRequest;
 import com.danielbukowski.photosharing.Dto.ChangePasswordRequest;
 import com.danielbukowski.photosharing.Dto.ImageDto;
@@ -13,89 +12,83 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
 @Validated
 @RestController
-@RequestMapping("api/v1/accounts")
+@RequestMapping("api/v2/accounts")
 public class AccountController {
 
     private final AccountService accountService;
-
-    @GetMapping
-    public ResponseEntity<List<AccountDto>> getAccounts() {
-        return ResponseEntity.ok(accountService.getAccounts());
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<AccountDto> getAccountById(@PathVariable UUID id) {
-        return ResponseEntity.ok(accountService.getAccountById(id));
-    }
 
     @PostMapping
     public ResponseEntity<?> createAccount(@RequestBody @Valid AccountRegisterRequest accountRegisterRequest) {
         UUID accountId = accountService.createAccount(accountRegisterRequest);
         return ResponseEntity
-                .created(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + accountId).build().toUri())
-                .build();
+                .created(
+                        ServletUriComponentsBuilder
+                                .fromCurrentRequest()
+                                .path("/%s".formatted(accountId))
+                                .build()
+                                .toUri()
+                ).build();
     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteAccountById(@PathVariable UUID id) {
-        accountService.deleteAccountById(id);
+    @DeleteMapping
+    public ResponseEntity<?> deleteAccount(@AuthenticationPrincipal Account account,
+                                           HttpServletRequest request) {
+        accountService.deleteAccountById(account.getId());
+        request.getSession().invalidate();
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/password")
-    public ResponseEntity<?> changePassword(Authentication authentication,
-                                            @Valid @RequestBody ChangePasswordRequest changePasswordRequest) {
-        accountService.changePassword(authentication.getName(), changePasswordRequest);
+    public ResponseEntity<?> changeAccountPassword(@AuthenticationPrincipal Account account,
+                                                   @Valid @RequestBody ChangePasswordRequest changePasswordRequest,
+                                                   HttpServletRequest request) {
+        accountService.changeAccountPassword(account, changePasswordRequest);
+        request.getSession().invalidate();
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/images")
-    public ResponseEntity<?> addImageToAccount(@Valid @Image MultipartFile image,
-                                               @AuthenticationPrincipal Account account) {
-        UUID imageId = accountService.saveImageToAccount(image, account.getUsername());
+    public ResponseEntity<?> addImageToAccount(@AuthenticationPrincipal Account account,
+                                               @Valid @Image MultipartFile image) {
+        UUID imageId = accountService.saveImageToAccount(image, account);
         return ResponseEntity
                 .created(
                         ServletUriComponentsBuilder
                                 .fromCurrentContextPath()
-                                .path("/api/v1/accounts/images/%s".formatted(imageId))
+                                .path("/api/v2/accounts/images/%s".formatted(imageId))
                                 .build()
                                 .toUri()
                 ).build();
     }
 
     @GetMapping("/images/{imageId}")
-    public ResponseEntity<byte[]> getImageFromAccount(@PathVariable UUID imageId,
-                                                      @AuthenticationPrincipal Account account) {
-        ImageDto imageDto = accountService.getImageFromAccount(account, imageId);
+    public ResponseEntity<byte[]> getImageFromAccount(@AuthenticationPrincipal Account account,
+                                                      @PathVariable UUID imageId) {
+        ImageDto imageDto = accountService.getImageFromAccount(account.getId(), imageId);
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .contentType(
-                        MediaType.parseMediaType("image/%s".formatted(imageDto.extension()))
-                )
+                .contentType(MediaType.valueOf(imageDto.contentType()))
                 .body(imageDto.data());
     }
 
     @DeleteMapping("/images/{imageId}")
-    public ResponseEntity<?> deleteImageFromAccount(@PathVariable UUID imageId,
-                                                    @AuthenticationPrincipal Account account) {
-        accountService.deleteImageFromAccount(account, imageId);
+    public ResponseEntity<?> deleteImageFromAccount(@AuthenticationPrincipal Account account,
+                                                    @PathVariable UUID imageId) {
+        accountService.deleteImageFromAccount(account.getId(), imageId);
         return ResponseEntity
                 .noContent()
                 .build();
     }
-
 
 }
