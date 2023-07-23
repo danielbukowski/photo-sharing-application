a3bac0065d62cc19e42c96098acb7a93fa91b3b8
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:34:05 +0200

    Add a default value to a field in the Accounts' table schema

--- src/main/resources/db/migration/V1__create_table_accounts.sql
+++ src/main/resources/db/migration/V1__create_table_accounts.sql
@@ -1,6 +1,5 @@
-
 CREATE TABLE accounts (
-   id UUID,
+   id UUID DEFAULT gen_random_uuid(),
    email VARCHAR(255) UNIQUE NOT NULL,
    password VARCHAR(255) NOT NULL,
    CONSTRAINT pk_accounts PRIMARY KEY (id)
