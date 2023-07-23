e18d817ac4f384b2e0f632dd96a75dbc6fb2a9a3
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:24:42 +0200

    Remove the getFromString method

--- src/main/java/com/danielbukowski/photosharing/Enum/FileExtension.java
+++ src/main/java/com/danielbukowski/photosharing/Enum/FileExtension.java
@@ -2,12 +2,7 @@ package com.danielbukowski.photosharing.Enum;
 
 public enum FileExtension {
     JPEG,
-    PNG;
+    PNG
+    ;
 
-    public static FileExtension getFromString(String string) {
-        for (FileExtension fileExtension : FileExtension.values()) {
-            if (fileExtension.name().equalsIgnoreCase(string)) return fileExtension;
-        }
-        throw new RuntimeException("Couldn't find an extension from the string: " + string);
-    }
 }
