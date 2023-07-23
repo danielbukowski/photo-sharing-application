4fcd1fab509b3f34439d329493aaca0fb70e8a34
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:13:03 +0200

    Add a deleteByAccountId method in the ImageRepository

--- src/main/java/com/danielbukowski/photosharing/Repository/ImageRepository.java
+++ src/main/java/com/danielbukowski/photosharing/Repository/ImageRepository.java
@@ -1,11 +1,23 @@
 package com.danielbukowski.photosharing.Repository;
 
 import com.danielbukowski.photosharing.Entity.Image;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, UUID> {
+
+    @Transactional
+    @Modifying
+    @Query(
+            "DELETE FROM Image i " +
+            "WHERE i.account.id = :accountId"
+    )
+    void deleteByAccountId(UUID accountId);
+
 }
