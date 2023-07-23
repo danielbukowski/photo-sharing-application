89aa51baf083abe75aa2060b1b189d42526ebfb5
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:12:13 +0200

    Remove id from the AccountDto

--- src/main/java/com/danielbukowski/photosharing/Dto/AccountDto.java
+++ src/main/java/com/danielbukowski/photosharing/Dto/AccountDto.java
@@ -3,11 +3,8 @@ package com.danielbukowski.photosharing.Dto;
 
 import lombok.Builder;
 
-import java.util.UUID;
-
 
 @Builder
 public record AccountDto (
-        UUID id,
         String email) {
 }
