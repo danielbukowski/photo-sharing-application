f1eaac07cc958b292736cbe50778e64d0a4e20ef
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:30:01 +0200

    Fix typo

--- src/main/java/com/danielbukowski/photosharing/Dto/ChangePasswordRequest.java
+++ src/main/java/com/danielbukowski/photosharing/Dto/ChangePasswordRequest.java
@@ -6,11 +6,11 @@ import org.hibernate.validator.constraints.Length;
 
 public record ChangePasswordRequest(
         @NotBlank(message = "Should not be empty")
-        @Length(min = 8, max = 32, message = "Should must be 8-32 characters long")
+        @Length(min = 8, max = 32, message = "Should be 8-32 characters long")
         @Pattern.List({
                 @Pattern(regexp = ".*[a-z].*", message = "Should have one lowercase letter"),
                 @Pattern(regexp = ".*[A-Z].*", message = "Should have one uppercase letter"),
-                @Pattern(regexp = ".*[@$!%*?&].*", message = "Should have one special character"),
+                @Pattern(regexp = ".*[@$!%*?&].*", message = "Should have one special character(@$!%*?&)"),
                 @Pattern(regexp = ".*\\d.*", message = "Should have one digit from 1 to 9"),
         })
         String newPassword) {
