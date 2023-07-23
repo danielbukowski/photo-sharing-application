719ab8d308ce62bb1c53b0ed097049aa19bc817f
danielbukowski <daniel.bukowski01@gmail.com>
2023-07-23 14:10:21 +0200

    Add 3 new methods to the AccountRepository

--- src/main/java/com/danielbukowski/photosharing/Repository/AccountRepository.java
+++ src/main/java/com/danielbukowski/photosharing/Repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package com.danielbukowski.photosharing.Repository;
 
 import com.danielbukowski.photosharing.Entity.Account;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,16 +14,40 @@ import java.util.UUID;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
+    @Transactional
+    @Modifying
+    @Query(
+            "UPDATE Account a " +
+            "SET a.password = :password " +
+            "WHERE a.id = :id"
+    )
+    void updatePasswordById(String password, UUID id);
 
-    @Query(name =
+    @Query(
+            "SELECT DISTINCT a " +
             "FROM Account a " +
-            "LEFT JOIN FETCH a.images  " +
-            "ON a.id = img.account_id " +
-            "WHERE LOWER(a.email) = LOWER(:email) "
+            "LEFT JOIN FETCH a.images i " +
+            "WHERE LOWER(a.email) = LOWER(:email)"
     )
     @EntityGraph(attributePaths = { "images" })
     Optional<Account> findByEmailIgnoreCase(String email);
 
+    @Override
+    @Query(
+            "SELECT a, i " +
+            "FROM Account a " +
+            "LEFT JOIN FETCH a.images i " +
+            "WHERE a.id = :id"
+    )
+    @EntityGraph(attributePaths = { "images" })
+    Optional<Account> findById(UUID id);
 
+    @Query(
+            "SELECT COUNT(a) > 0 " +
+            "THEN true ELSE false END " +
+            "FROM Account a " +
+            "WHERE LOWER(a.email) = LOWER(:email)"
+    )
+    boolean existsByEmailIgnoreCase(String email);
 
 }
