9d7936c2d3f82ed99cd0ccbc543264d788d706ee
danielbukowski <daniel.bukowski01@gmail.com>
2023-08-01 23:23:08 +0200

    Refactor the schema

--- src/main/resources/db/migration/V2__create_table_images.sql
+++ src/main/resources/db/migration/V2__create_table_images.sql
@@ -1,11 +1,9 @@
 CREATE TABLE images (
-   id UUID,
-   account_id UUID,
-   path VARCHAR(255) UNIQUE,
-   title VARCHAR(255) NOT NULL ,
-   extension VARCHAR(4) NOT NULL,
-   created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+   id UUID DEFAULT gen_random_uuid(),
+   account_id UUID NOT NULL,
+   title VARCHAR(255) NOT NULL,
+   content_type VARCHAR(10) NOT NULL,
+   create_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
    CONSTRAINT pk_images PRIMARY KEY (id),
-   CONSTRAINT fk_images_on_account FOREIGN KEY (account_id) REFERENCES accounts(id)
+   CONSTRAINT fk_images_accounts FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
 );
-
