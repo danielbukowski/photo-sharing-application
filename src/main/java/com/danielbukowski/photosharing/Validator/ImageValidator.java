9d315a768e3787362916a0756a10c4c495e02318
danielbukowski <daniel.bukowski01@gmail.com>
2023-08-01 23:27:22 +0200

    Improve the ImageValidator

--- src/main/java/com/danielbukowski/photosharing/Validator/ImageValidator.java
+++ src/main/java/com/danielbukowski/photosharing/Validator/ImageValidator.java
@@ -1,52 +1,62 @@
 package com.danielbukowski.photosharing.Validator;
 
-import com.danielbukowski.photosharing.Enum.FileExtension;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
 
-import static com.danielbukowski.photosharing.Enum.FileExtension.JPEG;
-import static com.danielbukowski.photosharing.Enum.FileExtension.PNG;
-
+@Slf4j
 public class ImageValidator implements ConstraintValidator<Image, MultipartFile> {
+
+    public static final String JPEG = "JPEG";
+    public static final String PNG = "PNG";
+    public static final String JPG = "JPG";
 
     @Override
     public boolean isValid(MultipartFile file, ConstraintValidatorContext constraintValidatorContext) {
         if (file == null) return false;
 
-        return isFileTypeValid(file)
-                && isFileExtensionValid(file)
-                && isContentTypeValid(file);
+        return isFileExtensionValid(file)
+                && isContentTypeValid(file)
+                && isFileTypeValid(file);
     }
 
     private boolean isFileExtensionValid(MultipartFile multipartFile) {
         String fileExtension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
-        return JPEG.name().equalsIgnoreCase(fileExtension) || PNG.name().equalsIgnoreCase(fileExtension);
+        log.info("Found a file with a file extension {}", fileExtension);
+        return JPEG.equalsIgnoreCase(fileExtension)
+                || JPG.equalsIgnoreCase(fileExtension)
+                || PNG.equalsIgnoreCase(fileExtension);
     }
 
     private boolean isContentTypeValid(MultipartFile multipartFile) {
         String fileContentType = multipartFile.getContentType();
+        log.info("Found a file with a content type {}", fileContentType);
         return Objects.equals(fileContentType, MediaType.IMAGE_JPEG_VALUE)
-                || Objects.equals(fileContentType, MediaType.IMAGE_PNG_VALUE);
+                || Objects.equals(fileContentType, MediaType.IMAGE_PNG_VALUE)
+                ;
     }
 
     @SneakyThrows
     private boolean isFileTypeValid(MultipartFile multipartFile) {
-        FileExtension fileType = getFileType(multipartFile.getBytes());
-        return fileType != null && (fileType.equals(PNG) || fileType.equals(JPEG));
+        String fileType = getFileType(multipartFile.getBytes());
+        log.info("Found a file with a file type {}", fileType);
+        return fileType != null
+                && (fileType.equals(PNG)
+                || fileType.equals(JPEG));
     }
 
     // Checks Magic Bytes in a file
-    private FileExtension getFileType(byte[] fileData) {
-        // Finds '‰P' signature
+    private String getFileType(byte[] fileData) {
+        // Finds the '‰P' signature
         if (Byte.toUnsignedInt(fileData[0]) == 0x89 && Byte.toUnsignedInt(fileData[1]) == 0x50)
             return PNG;
-            // Finds 'ÿØ' signature
+            // Finds the 'ÿØ' signature
         else if (Byte.toUnsignedInt(fileData[0]) == 0xFF && Byte.toUnsignedInt(fileData[1]) == 0xD8)
             return JPEG;
         return null;
