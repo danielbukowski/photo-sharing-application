e159437b9e1072829402ae663a7f418a06fd29d0
danielbukowski <daniel.bukowski01@gmail.com>
2023-08-12 17:23:32 +0200

    Add role relationship to the Account entity

--- src/main/java/com/danielbukowski/photosharing/Entity/Account.java
+++ src/main/java/com/danielbukowski/photosharing/Entity/Account.java
@@ -3,6 +3,7 @@ package com.danielbukowski.photosharing.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.*;
@@ -41,9 +42,26 @@ public class Account implements UserDetails {
     )
     private List<Image> images = new ArrayList<>();
 
+    @ManyToMany(
+            cascade = {CascadeType.MERGE, CascadeType.PERSIST}
+    )
+    @JoinTable(
+            name = "accounts_roles",
+            joinColumns = @JoinColumn(name = "account_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+        role.getAccounts().add(this);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles.stream()
+                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.getName()))
+                .toList();
     }
 
     @Override
