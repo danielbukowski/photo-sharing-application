52590ae864795613507651ce82742b94348eea10
danielbukowski <daniel.bukowski01@gmail.com>
2023-08-25 18:11:26 +0200

    Add isEmailVerified field to the Account

--- src/main/resources/db/migration/V1__create_table_accounts.sql
+++ src/main/resources/db/migration/V1__create_table_accounts.sql
@@ -2,5 +2,6 @@ CREATE TABLE accounts (
    id UUID DEFAULT gen_random_uuid(),
    email VARCHAR(255) UNIQUE NOT NULL,
    password VARCHAR(255) NOT NULL,
+   is_email_verified BOOLEAN DEFAULT FALSE NOT NULL,
    CONSTRAINT pk_accounts PRIMARY KEY (id)
 );
