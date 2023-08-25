f28f68b4f3cbf92de510eecee99a035691185dce
danielbukowski <daniel.bukowski01@gmail.com>
2023-08-25 18:10:29 +0200

    Add the EmailVerificationToken schema

---
+++ src/main/resources/db/migration/V5__create_email_verification_token_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE email_verification_tokens (
+    id UUID DEFAULT gen_random_uuid(),
+    account_id UUID NOT NULL,
+    expiration_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT pk_email_verification_tokens PRIMARY KEY (id),
+    CONSTRAINT fk_email_verification_tokens_accounts FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
+);
