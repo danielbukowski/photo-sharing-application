eebe61b63c82fe90934a51d65f9daff738c1840b
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-14 17:53:16 +0200

    Fix typo

--- src/main/resources/db/migration/V6__create_table_comments.sql
+++ src/main/resources/db/migration/V6__create_table_comments.sql
@@ -4,6 +4,6 @@ CREATE TABLE comments(
     image_id UUID NOT NULL,
     comment_account_id UUID NOT NULL,
     CONSTRAINT pk_comments PRIMARY KEY (id),
-    CONSTRAINT fk_images FOREIGN KEY (image_id) REFERENCES images(id),
-    CONSTRAINT fk_accounts FOREIGN KEY (account_id) REFERENCES accounts(id)
+    CONSTRAINT fk_images FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE,
+    CONSTRAINT fk_accounts FOREIGN KEY (comment_account_id) REFERENCES accounts(id)
 );
