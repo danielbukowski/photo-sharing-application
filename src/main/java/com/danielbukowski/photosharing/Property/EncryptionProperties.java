d1242cb9b803991c5f820bda5c11b6a613185eb8
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-15 17:02:36 +0200

    Add the encryption properties

---
+++ src/main/java/com/danielbukowski/photosharing/Property/EncryptionProperties.java
@@ -0,0 +1,14 @@
+package com.danielbukowski.photosharing.Property;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+@ConfigurationProperties(prefix = "encryption")
+public class EncryptionProperties {
+
+    private String password;
+    private String salt;
+}
