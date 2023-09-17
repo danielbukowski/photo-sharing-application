7130cbbf73f0147d5712401065a40778297c3574
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-17 20:01:10 +0200

    Add 3 endpoints in the imageController to add, delete, and get likes

--- src/main/java/com/danielbukowski/photosharing/Controller/ImageController.java
+++ src/main/java/com/danielbukowski/photosharing/Controller/ImageController.java
@@ -1,8 +1,8 @@
 package com.danielbukowski.photosharing.Controller;
 
 import com.danielbukowski.photosharing.Dto.CommentDto;
-import com.danielbukowski.photosharing.Dto.NewCommentRequest;
 import com.danielbukowski.photosharing.Dto.ImageDto;
+import com.danielbukowski.photosharing.Dto.NewCommentRequest;
 import com.danielbukowski.photosharing.Dto.SimplePageResponse;
 import com.danielbukowski.photosharing.Entity.Account;
 import com.danielbukowski.photosharing.Service.CommentService;
@@ -17,6 +17,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -65,6 +66,35 @@ public class ImageController {
     public ResponseEntity<SimplePageResponse<UUID>> getListOfLatestImages(@RequestParam(required = false, defaultValue = "0") Integer pageNumber) {
         pageNumber = Integer.max(0, pageNumber);
         return ResponseEntity.ok(imageService.getIdsOfLatestImages(pageNumber));
+    }
+
+    @PostMapping("/{imageId}/likes")
+    public ResponseEntity<?> addLikeToImage(@AuthenticationPrincipal Account account,
+                                            @PathVariable UUID imageId) {
+        imageService.addLikeToImage(imageId, account);
+        return ResponseEntity.created(
+                        ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("api/v1/images/%s/likes".formatted(imageId))
+                                .build()
+                                .toUri()
+                )
+                .build();
+    }
+
+    @GetMapping("/{imageId}/likes")
+    public ResponseEntity<?> getNumberOfLikesFromImage(@AuthenticationPrincipal Account account,
+                                                       @PathVariable UUID imageId) {
+        return ResponseEntity.ok(Map.of("likes", imageService.getNumberOfLikesFromImage(imageId, account)));
+    }
+
+    @DeleteMapping("/{imageId}/likes")
+    public ResponseEntity<?> removeLikeFromImage(@AuthenticationPrincipal Account account,
+                                                 @PathVariable UUID imageId) {
+        imageService.removeLikeFromImage(imageId, account);
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 
 }
