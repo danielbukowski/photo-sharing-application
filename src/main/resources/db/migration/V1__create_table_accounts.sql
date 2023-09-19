b16cd1b2a76d5cbf08c2e14684d2b0eaa70dfc49
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-19 19:02:58 +0200

    Refactor the Account entity

--- src/main/resources/db/migration/V1__create_table_accounts.sql
+++ src/main/resources/db/migration/V1__create_table_accounts.sql
@@ -1,7 +1,8 @@
 CREATE TABLE accounts (
-   id UUID DEFAULT gen_random_uuid(),
-   email VARCHAR(255) UNIQUE NOT NULL,
+   account_id UUID DEFAULT gen_random_uuid(),
+   email VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
    is_email_verified BOOLEAN DEFAULT FALSE NOT NULL,
-   CONSTRAINT pk_accounts PRIMARY KEY (id)
+   CONSTRAINT pk_accounts PRIMARY KEY (account_id),
+   CONSTRAINT uq_accounts_email UNIQUE (email)
 );
