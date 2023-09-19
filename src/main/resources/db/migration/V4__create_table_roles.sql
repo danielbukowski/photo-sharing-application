605d6b51f717cbd18a4fb2cb26987cf8ff2a3b63
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-19 19:03:27 +0200

    Refactor the Role entity

--- src/main/resources/db/migration/V4__create_table_roles.sql
+++ src/main/resources/db/migration/V4__create_table_roles.sql
@@ -1,14 +1,14 @@
-CREATE TABLE roles(
-    id UUID DEFAULT gen_random_uuid(),
+CREATE TABLE roles (
+    role_id UUID DEFAULT gen_random_uuid(),
     name varchar(255) UNIQUE NOT NULL,
-    CONSTRAINT pk_roles PRIMARY KEY (id)
+    CONSTRAINT pk_roles PRIMARY KEY (role_id)
 );
 
-CREATE TABLE accounts_roles(
-    account_id UUID,
-    role_id UUID,
-    CONSTRAINT fk_accounts FOREIGN KEY(account_id) REFERENCES accounts(id),
-    CONSTRAINT fk_roles FOREIGN KEY(role_id) REFERENCES roles(id)
+CREATE TABLE accounts_roles (
+    account_id UUID NOT NULL,
+    role_id UUID NOT NULL,
+    CONSTRAINT fk_accounts_accounts_roles FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT fk_roles_accounts_roles FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO roles(name) VALUES ('USER'), ('ADMIN');
