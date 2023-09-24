fecef6160c36e0644d667987efa7c4b704dfab16
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-24 23:54:27 +0200

    Add the AccountUpdateRequest

---
+++ src/main/java/com/danielbukowski/photosharing/Dto/AccountUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.danielbukowski.photosharing.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+public record AccountUpdateRequest(
+        @NotBlank(message = "Should not be blank")
+        @Pattern(regexp = ".*[a-z]{4}.*", message = "Should have at 4 letters")
+        @Length(min = 4, message = "The length should be at least 4 characters")
+        String nickname,
+        String biography) {
+}
