6d758656b37d053cb187dc4ee04c71eeb4c42189
danielbukowski <daniel.bukowski01@gmail.com>
2023-09-25 14:56:06 +0200

    Add the getAccountDetails method in the AccountService

--- src/main/java/com/danielbukowski/photosharing/Service/AccountService.java
+++ src/main/java/com/danielbukowski/photosharing/Service/AccountService.java
@@ -1,19 +1,24 @@
 package com.danielbukowski.photosharing.Service;
 
+import com.danielbukowski.photosharing.Dto.AccountDto;
 import com.danielbukowski.photosharing.Dto.AccountRegisterRequest;
-import com.danielbukowski.photosharing.Dto.ChangePasswordRequest;
+import com.danielbukowski.photosharing.Dto.PasswordChangeRequest;
 import com.danielbukowski.photosharing.Entity.Account;
 import com.danielbukowski.photosharing.Exception.AccountAlreadyExistsException;
 import com.danielbukowski.photosharing.Exception.InvalidPasswordException;
 import com.danielbukowski.photosharing.Repository.AccountRepository;
+import com.danielbukowski.photosharing.Repository.EmailVerificationTokenRepository;
 import com.danielbukowski.photosharing.Repository.ImageRepository;
 import com.danielbukowski.photosharing.Repository.RoleRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.danielbukowski.photosharing.Enum.ExceptionMessageResponse.ACCOUNT_WITH_ALREADY_EXISTING_EMAIL;
@@ -87,6 +92,38 @@ public class AccountService {
         }
 
         accountRepository.updatePasswordById(passwordEncoder.encode(newPassword), account.getId());
+    }
+
+    public AccountDto getAccountDetails(Account account) {
+        var accountDetails = AccountDto.builder();
+        if (account.isEmailVerified()) {
+            accountDetails.accountVerifiedAt(
+                    emailVerificationTokenRepository.findByAccountId(
+                            account.getId()).get().getVerifiedAt()
+            );
+        }
+        List<String> roles = new ArrayList<>();
+        List<String> permissions = new ArrayList<>();
+        accountDetails.isEmailVerified(account.isEmailVerified())
+                .accountId(account.getId())
+                .nickname(account.getNickname())
+                .email(account.getEmail())
+                .biography(account.getBiography())
+                .roles(roles)
+                .permissions(permissions)
+                .isEmailVerified(account.isEmailVerified())
+                .numberOfUploadedImages(imageRepository.findByAccountId(account.getId()).get().size())
+                .isLocked(account.isLocked());
+
+        for (GrantedAuthority authority : account.getAuthorities()) {
+            if (authority.getAuthority().startsWith("ROLE_")) {
+                roles.add(authority.getAuthority());
+                continue;
+            }
+            permissions.add(authority.getAuthority());
+        }
+
+        return accountDetails.build();
     }
 
 }
