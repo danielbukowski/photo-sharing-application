0180580164489d164eedf3163ff5b6ace953b537
danielbukowski <daniel.bukowski01@gmail.com>
2023-10-28 23:09:14 +0200

    Add the login service

---
+++ photo-sharing-application-front-end/src/app/login/login.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Credentials } from '../model/credentials';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { Buffer } from 'buffer';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor(private http: HttpClient) {}
+
+  login(credentials: Credentials): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization:'Basic ' + Buffer.from(`${credentials.email}:${credentials.password}`).toString('base64')
+      })
+    };
+    return this.http.post(
+      'http://localhost:8081/api/v1/sessions',
+      {},
+      httpOptions
+    );
+  }
+}
