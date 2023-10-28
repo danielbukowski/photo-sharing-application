161b09f47dad52ad8054bde1ef3ff47183cbe1a6
danielbukowski <daniel.bukowski01@gmail.com>
2023-10-28 23:03:28 +0200

    Add the HttpCsrfTokenInterceptor

---
+++ photo-sharing-application-front-end/src/app/Interceptor/HttpCsrfTokenInterceptor.ts
@@ -0,0 +1,26 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { CsrfTokenService } from '../service/csrf-token.service';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpCsrfTokenInterceptor implements HttpInterceptor {
+
+  constructor(private csrfTokenService: CsrfTokenService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const respHeaderName = 'X-XSRF-TOKEN';
+    let token = this.csrfTokenService.getCsrfToken;
+
+    if (!(req.method === "GET" || req.method === "HEAD") && token) {
+      req = req.clone({ headers: req.headers.set(respHeaderName, token) });
+    }
+    return next.handle(req);
+  }
+
+}
