62b14bc942227ddc21293e61aef7fc23da7de59b
danielbukowski <daniel.bukowski01@gmail.com>
2023-10-28 23:04:42 +0200

    Fix pattern validation in the dto

--- src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
+++ src/main/java/com/danielbukowski/photosharing/Dto/AccountRegisterRequest.java
@@ -10,7 +10,7 @@ public record AccountRegisterRequest(
         @Email
         String email,
         @NotBlank(message = "Should not be blank")
-        @Pattern(regexp = "(.*[a-zA-z]){4}", message = "Should have at 4 letters")
+        @Pattern(regexp = "(.*[a-zA-z].*){4}", message = "Should have at 4 letters")
         @Length(min = 4, message = "The length should be at least 4 characters")
         String nickname,
         @NotBlank(message = "Should not be blank")
