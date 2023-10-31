101780a314e92107d591c406d9cf2559bd612bdb
danielbukowski <daniel.bukowski01@gmail.com>
2023-10-31 23:36:02 +0100

    Add the csrfTokenService

---
+++ photo-sharing-application-front-end/src/app/csrf-token/csrf-token.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CsrfTokenService {
+
+  private token = signal<string>("");
+
+  constructor(private http: HttpClient) { }
+
+  get getCsrfToken(): string {
+    return this.token() as string;
+  }
+
+  generateCsrfToken() {
+    return this.http
+      .get('http://localhost:8081/api/v1/csrf')
+      .subscribe(
+      (resp: any) => this.token.set(resp.data.token)
+      );
+  }
+
+}
