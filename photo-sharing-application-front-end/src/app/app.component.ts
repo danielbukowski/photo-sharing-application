d6d52c288fdb2c9726870103cf25613b4ede6135
danielbukowski <daniel.bukowski01@gmail.com>
2023-10-31 23:39:41 +0100

    Add a functionality to generate a csrf token on the start of the app

--- photo-sharing-application-front-end/src/app/app.component.ts
+++ photo-sharing-application-front-end/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CsrfTokenService } from './csrf-token/csrf-token.service';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,18 @@ import { Component, OnChanges, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+
+  constructor(private CsrfTokenService: CsrfTokenService) {}
+
   ngOnInit(): void {
-    if (
-      localStorage.getItem('theme') === 'dark' ||
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
+    if (localStorage.getItem('theme') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
       localStorage.setItem('theme', 'dark');
       document.documentElement.classList.add('dark');
     } else {
       localStorage.setItem('theme', 'light');
       document.documentElement.classList.remove('dark');
     }
+    this.CsrfTokenService.generateCsrfToken();
   }
+
 }
