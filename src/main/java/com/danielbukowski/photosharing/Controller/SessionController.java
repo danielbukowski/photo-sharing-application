d5def7310ebc8f1b588397c20a24b33c83861426
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-02 12:11:51 +0100

    Add a basic documentation to the SessionController

--- src/main/java/com/danielbukowski/photosharing/Controller/SessionController.java
+++ src/main/java/com/danielbukowski/photosharing/Controller/SessionController.java
@@ -1,5 +1,8 @@
 package com.danielbukowski.photosharing.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,20 +15,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/sessions")
 public class SessionController {
 
-        @PostMapping
-        public ResponseEntity<?> logIn() {
-            return ResponseEntity
-                    .ok()
-                    .build();
-        }
+    @SecurityRequirement(name = "Basic auth")
+    @Operation(
+            summary = "Create a valid session",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "A valid session has been created"
+                    )
+            }
+    )
+    @PostMapping
+    public ResponseEntity<?> logIn() {
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 
-        @DeleteMapping
-        public ResponseEntity<?> logOut(HttpServletRequest httpRequest) {
-            httpRequest.getSession().invalidate();
-            SecurityContextHolder.clearContext();
-            return ResponseEntity
-                    .noContent()
-                    .build();
-        }
+    @SecurityRequirement(name = "Basic auth")
+    @Operation(
+            summary = "Delete a valid session",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "A valid session has been deleted"
+                    )
+            }
+    )
+    @DeleteMapping
+    public ResponseEntity<?> logOut(HttpServletRequest httpRequest) {
+        httpRequest.getSession().invalidate();
+        SecurityContextHolder.clearContext();
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
 
 }
