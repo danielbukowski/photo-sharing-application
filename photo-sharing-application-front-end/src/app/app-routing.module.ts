3c2737583b7b3ce643f7c478566a7d9617fa5325
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-06 00:16:02 +0100

    Add a basic routing

--- photo-sharing-application-front-end/src/app/app-routing.module.ts
+++ photo-sharing-application-front-end/src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { ForgottenPasswordComponent } from './forgotten-password/forgotten-password.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { VerificationComponent } from './verification/verification.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  },
+  {
+    path: 'forget-password',
+    component: ForgottenPasswordComponent,
+  },
+  {
+    path: 'verify',
+    component: VerificationComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
