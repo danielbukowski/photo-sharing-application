4d1ef98a59ce65e48ba7fd72db370f272a7e8dfb
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-06 22:45:08 +0100

    Refactor the app component

--- photo-sharing-application-front-end/src/app/app.component.ts
+++ photo-sharing-application-front-end/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CsrfTokenService } from './csrf-token/csrf-token.service';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,15 @@ import { CsrfTokenService } from './csrf-token/csrf-token.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private CsrfTokenService: CsrfTokenService) {}
+  constructor(private CsrfTokenService: CsrfTokenService, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authService.updateAuthentication();
+    this.setTheme();
+    this.CsrfTokenService.generateCsrfToken();
+  }
+
+  private setTheme() {
     if (localStorage.getItem('theme') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
       localStorage.setItem('theme', 'dark');
       document.documentElement.classList.add('dark');
@@ -18,7 +25,6 @@ export class AppComponent implements OnInit {
       localStorage.setItem('theme', 'light');
       document.documentElement.classList.remove('dark');
     }
-    this.CsrfTokenService.generateCsrfToken();
   }
 
 }
