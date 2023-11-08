0d9fca9a91ba5381b8e007f1a515b07207ff6d26
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-08 16:14:58 +0100

    Update the auth service

--- photo-sharing-application-front-end/src/app/auth/auth.service.ts
+++ photo-sharing-application-front-end/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, Signal, WritableSignal, signal } from '@angular/core';
 import { Account } from '../model/account';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, filter, map } from 'rxjs';
@@ -6,33 +6,26 @@ import { BehaviorSubject, Observable, filter, map } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-// DISCLAIMER: STORE ONLY OBSERVABLE
 export class AuthService {
-  private accountDetails$ = new BehaviorSubject<Account | undefined>(undefined);
+  #accountDetails = signal<Account | undefined>(undefined);
 
-  constructor(private http: HttpClient) {
-   }
+  constructor(private http: HttpClient) {}
+
+  getAccountDetails(): Signal<Account | undefined> {
+    return this.#accountDetails.asReadonly();
+  }
 
   updateAuthentication(): void {
     this.http
     .get<Account | undefined>('http://localhost:8081/api/v3/accounts')
     .subscribe({
-      next: (n) => this.accountDetails$.next(n),
-      error: (e) => this.accountDetails$.next(undefined),
+      next: (n) => this.#accountDetails.set(n),
+      error: (e) => this.#accountDetails.set(undefined),
     });
   }
 
-  isAuthenticated(): Observable<boolean> {
-    return this.accountDetails$.pipe(
-      filter((account) => {
-        return account !== undefined;
-      }),
-      map((account) => {
-        if (account) {
-          return true;
-        }
-        return false;
-      })
-    );
+  isAuthenticated(): boolean {
+    return this.#accountDetails() !== undefined;
   }
+  
 }
