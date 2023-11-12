c00abb4c138168af13d78c94e1e7c373bc8c68a6
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-12 23:25:32 +0100

    Generate the add-image-page component

---
+++ photo-sharing-application-front-end/src/app/add-image-page/add-image-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-image-page',
+  templateUrl: './add-image-page.component.html',
+  styleUrls: ['./add-image-page.component.css']
+})
+export class AddImagePageComponent {
+
+}
