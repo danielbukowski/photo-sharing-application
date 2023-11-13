2438894016496559c068bc6a20e029913a89c879
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-13 23:25:19 +0100

    Display images from the backend on the home page

--- photo-sharing-application-front-end/src/app/home/home.component.ts
+++ photo-sharing-application-front-end/src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ImageService } from '../image/image.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  listOfImagesAsIds!: Array<string>;
+
+  constructor(private imageService: ImageService) { }
+
+  ngOnInit(): void {
+    this.imageService.getLatestImagesInFormOfIdList()
+    .subscribe({
+      next: d => this.listOfImagesAsIds = d.data
+    });
+  }
 
 }
