c51a308318b4516518a60187323cdda02fd51c27
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-21 16:20:33 +0100

    Add methods to add and get a comment from image in the comment.service.ts

--- photo-sharing-application-front-end/src/app/comment/comment.service.ts
+++ photo-sharing-application-front-end/src/app/comment/comment.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Page } from '../model/page';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addCommentToImage(content: string, imageId: string): Observable<any> {
+    return this.http.post(`http://localhost:8081/api/v1/images/${imageId}/comments`, {content});
+  }
+
+  getCommentsFromImage(imageId: string, pageNumber: number): Observable<Page<any>> {
+    return this.http.get<Page<Comment>>(`http://localhost:8081/api/v1/images/${imageId}/comments`, {params: {pageNumber}});
+  }
+
 }
