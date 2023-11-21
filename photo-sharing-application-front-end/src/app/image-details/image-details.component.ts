d4dd30b737b83179252ae4fe7274a770a69557cd
danielbukowski <daniel.bukowski01@gmail.com>
2023-11-21 16:12:45 +0100

    Refactor the onInit function in the image details component

--- photo-sharing-application-front-end/src/app/image-details/image-details.component.ts
+++ photo-sharing-application-front-end/src/app/image-details/image-details.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Signal, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ImageService } from '../image/image.service';
+import { Comment } from '../model/comment';
+import { Page } from '../model/page';
+import { CommentService } from '../comment/comment.service';
+import { AuthService } from '../auth/auth.service';
+import { Account } from '../model/account';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-image-details',
   templateUrl: './image-details.component.html',
-  styleUrls: ['./image-details.component.css']
+  styleUrls: ['./image-details.component.css'],
 })
-export class ImageDetailsComponent {
+export class ImageDetailsComponent implements OnInit {
+  IdOfCurrentDisplayedImage = signal<string>("");
+  numberOfLikes = signal<number>(0);
+  accountDetails!: Signal<Account | undefined>;
+  commentPage$!: Observable<Page<Comment>>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private imageService: ImageService,
+    private commentService: CommentService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({next: params => {
+      this.IdOfCurrentDisplayedImage.set(params.get('id') || "");
+    }});
+
+    this.accountDetails = this.authService.getAccountDetails();
+
+    this.imageService
+    .getNumberOfLikesFromImage(this.IdOfCurrentDisplayedImage())
+    .subscribe({
+      next: (d) => {
+        this.numberOfLikes.set(d.data.likes);
+      },
+    });
+
+  }
 
 }
