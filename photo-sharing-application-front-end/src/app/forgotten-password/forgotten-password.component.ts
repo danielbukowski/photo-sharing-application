efde4654441d066adaf9c458d08cac6e1d0c55ed
danielbukowski <daniel.bukowski01@gmail.com>
2023-12-01 22:28:51 +0100

    Refactor the forgotten-password.component.ts file

--- photo-sharing-application-front-end/src/app/forgotten-password/forgotten-password.component.ts
+++ photo-sharing-application-front-end/src/app/forgotten-password/forgotten-password.component.ts
@@ -1,32 +1,61 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ForgottenPasswordService } from './forgotten-password.service';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './forgotten-password.component.html',
-  styleUrls: ['./forgotten-password.css']
+  styleUrls: ['./forgotten-password.css'],
 })
-export class ForgottenPasswordComponent {
+export class ForgottenPasswordComponent implements OnDestroy {
+  validationError$: BehaviorSubject<string> = new BehaviorSubject('');
+  generalError$: BehaviorSubject<string> = new BehaviorSubject('');
+  isBeingProcessed$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  forgetPasswordForm!: FormGroup;
 
-  constructor(private forgottenPasswordService: ForgottenPasswordService, private router: Router) { }
+  constructor(
+    private forgottenPasswordService: ForgottenPasswordService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
-  errorReason: string = "";
+  ngOnInit(): void {
+    this.forgetPasswordForm = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
 
-  onSubmit(form: NgForm) {
-      this.forgottenPasswordService.sendPasswordResetRequest(form.value).subscribe({
+  ngOnDestroy(): void {
+    this.validationError$.unsubscribe();
+    this.generalError$.unsubscribe();
+    this.isBeingProcessed$.unsubscribe();
+  }
+
+  resetForm(): void {
+    this.forgetPasswordForm.reset();
+    this.validationError$.next('');
+    this.generalError$.next('');
+  }
+
+  onSubmit() {
+    this.isBeingProcessed$.next(true);
+    this.forgottenPasswordService
+      .sendPasswordResetRequest(this.forgetPasswordForm.value)
+      .subscribe({
         next: (n) => {
-          this.router.navigate(["/login"]);
-        }, 
-        error: err => {
-          if(err.status === 404) {
-            this.errorReason = "The email has been not found"
+          this.router.navigate(['/login']);
+        },
+        error: (e) => {
+          this.resetForm();
+          if (e.error.fieldNames) {
+            this.validationError$.next(e.error.fieldNames.email);
           } else {
-            this.errorReason = err.error.fieldNames.email;
+            this.generalError$.next(e.error.reason);
           }
-         form.resetForm();
-        }}
-      )
+          this.isBeingProcessed$.next(false);
+        },
+      });
   }
 }
