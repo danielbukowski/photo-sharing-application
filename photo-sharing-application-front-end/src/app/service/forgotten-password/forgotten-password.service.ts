f505330d8474dd878644972f7d8486e61936a467
danielbukowski <daniel.bukowski01@gmail.com>
2023-12-09 16:30:25 +0100

    Format the forgotten-password.service.ts file

--- photo-sharing-application-front-end/src/app/service/forgotten-password/forgotten-password.service.ts
+++ photo-sharing-application-front-end/src/app/service/forgotten-password/forgotten-password.service.ts
@@ -1,16 +1,20 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ForgottenPasswordService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  sendPasswordResetRequest(email: string) {
+  sendPasswordResetRequest(email: string): Observable<any> {
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
-    return this.http.post("http://localhost:8081/api/v3/accounts/password-reset", email, {headers: headers});
+    return this.http.post(
+      'http://localhost:8081/api/v3/accounts/password-reset',
+      email,
+      { headers: headers }
+    );
   }
 }
