1ee1d3a6a4cd410d0dfa3d9e75c6b3eea4b030ba
danielbukowski <daniel.bukowski01@gmail.com>
2023-12-11 14:47:42 +0100

    Rename a description in the registration-page.component.spec.ts file

--- photo-sharing-application-front-end/src/app/registration/registration-page.component.spec.ts
+++ photo-sharing-application-front-end/src/app/registration/registration-page.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrationPageComponent } from './registration-page.component';
 
-describe('RegisterPageComponent', () => {
+describe('RegistrationPageComponent', () => {
   let component: RegistrationPageComponent;
   let fixture: ComponentFixture<RegistrationPageComponent>;
 
