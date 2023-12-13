4e3bffb47d907c5df38c52e7bd08ece3c969ef1a
danielbukowski <daniel.bukowski01@gmail.com>
2023-12-13 23:54:31 +0100

    Move the all init methods from the account component to the APP_INITIALIZER method

--- photo-sharing-application-front-end/src/app/app.module.ts
+++ photo-sharing-application-front-end/src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home/home-page.component';
@@ -9,13 +8,45 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationPageComponent } from './registration/registration-page.component';
 import { VerificationPageComponent } from './verification/verification-page.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpCookieInterceptor } from './Interceptors/HttpCookieInterceptor';
 import { HttpCsrfTokenInterceptor } from './Interceptors/HttpCsrfTokenInterceptor';
 import { ForgottenPasswordPageComponent } from './forgotten-password/forgotten-password-page.component';
 import { ImageUploaderPageComponent } from './image-uploader/image-uploader-page.component';
 import { ImageDetailPageComponent } from './image-detail/image-detail-page.component';
+import { AuthService } from './services/auth/auth.service';
+import { CsrfTokenService } from './services/csrf-token/csrf-token.service';
+
+export function initApp(
+  authService: AuthService,
+  CsrfTokenService: CsrfTokenService
+) {
+  return (): Promise<void> => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          localStorage.getItem('theme') === 'dark' ||
+          window.matchMedia('(prefers-color-scheme: dark)').matches
+        ) {
+          localStorage.setItem('theme', 'dark');
+          document.documentElement.classList.add('dark');
+        } else {
+          localStorage.setItem('theme', 'light');
+          document.documentElement.classList.remove('dark');
+        }
+
+        CsrfTokenService.updateCsrfToken();
+        authService.updateAuthentication();
+        resolve();
+      });
+    });
+  };
+}
 
 @NgModule({
   declarations: [
@@ -35,7 +66,7 @@ import { ImageDetailPageComponent } from './image-detail/image-detail-page.compo
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     {
@@ -47,6 +78,12 @@ import { ImageDetailPageComponent } from './image-detail/image-detail-page.compo
       provide: HTTP_INTERCEPTORS,
       useClass: HttpCsrfTokenInterceptor,
       multi: true,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      multi: true,
+      deps: [AuthService, CsrfTokenService, HttpClient],
     },
   ],
   bootstrap: [AppComponent],
